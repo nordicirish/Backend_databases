8f5af345b1fc2cc63ca1748211052a470bb5a903
Roger Graham <roger.graham@tuni.fi>
2022-02-07 16:21:47 +0200

    Task1.sql Add answer

--- exercise_3/task1.sql
+++ exercise_3/task1.sql
@@ -3,3 +3,14 @@
 -- that are ordered to New York. Include customer's first and last name, company and
 -- the order's shipping date into the result. Make sure orders which are not shipped yet
 -- are included in the result.
+SELECT
+  ship_city as "City",
+  shipped_date as "Shipped Date",
+  customers.first_name as "First Name",
+  customers.last_name as "Last Name",
+  customers.company as "Company"
+FROM
+  orders
+  LEFT JOIN customers ON customers.id = orders.customer_id
+WHERE
+  ship_city = "New York"
